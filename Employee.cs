92afd91edb52bd3740341d71e6f05466ba3e15ce
Alberto <unumnio@gmail.com>
2023-01-22 21:25:05 -0600

    create Class Date, Class Employee, Class SalaryEmployee and Class CommissionEmployee

---
+++ Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsPoo
+{
+    public abstract class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Date HiringDate { get; set; }
+        public Date BirthDate { get; set; }
+        public bool IsActive { get; set; }
+
+        public abstract decimal GetValueToPay();
+        public override string ToString()
+        {
+            return $"{Id}-{FirstName} {LastName}"+
+                $"\n\tBirth:...................{$"{BirthDate}",18}"+
+                $"\n\tHiring Date:.............{$"{HiringDate}",18}"+
+                $"\n\tIs Active:...............{$"{BirthDate}",18}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConceptsPoo;
+
+Console.WriteLine("Concepts POO");
+Console.WriteLine("************");
+
+Employee employee1 = new SalaryEmployee()
+{
+    Id = 10,
+    FirstName = "Luis",
+    LastName = "Laverde",
+    BirthDate=new Date(2010,12,31),
+    HiringDate=new Date(2023,1,01),
+    IsActive = true,
+    Salary=6000000.50M
+};
+Console.WriteLine(employee1);
+Employee employee2 = new CommissionEmployee()
+{
+    Id = 20,
+    FirstName = "Laura",
+    LastName = "Benavidez",
+    BirthDate=new Date(2012,2,27),
+    HiringDate=new Date(2022,5,10),
+    IsActive = true,
+    CommissionPercentaje= 0.03F,
+    Sales=380000000M
+};
+Console.WriteLine(employee2);
